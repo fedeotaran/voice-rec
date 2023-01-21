89f850992197df1e73179a806f6badf01a70b936
Fede Otaran <otaran.federico@gmail.com>
2023-01-20 22:56:31 -0300

    Agrego más tiempo de reconocimiento de voz

--- voice_to_text.py
+++ voice_to_text.py
@@ -3,12 +3,12 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    # read the audio data from the default microphone
+
     print("Grabando...")
-    audio_data = r.record(source, duration=10)
+    audio_data = r.record(source, duration=30)
+
     print("Reconociendo...")
-    # convert speech to text
     text = r.recognize_google(audio_data, language="es-ES")
     
-    print("Texto reconocido.")
+    print("Texto reconocido:")
     print(text)
